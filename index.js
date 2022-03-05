210b75dc9a2581f6b6afbff6a08d712bceba0cfd
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-03-05 18:23:31 +0300

    st:This will change the colors

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ let theYear = document.getElementById("year");
 
 let displayAkan = document.querySelector(".akaname");
 
+// function validate() {
+//     var genders = document.getElementsById("gender");
+//     if( document.form.year.value == "" || document.form.theYear.value.length !=4 || document.form.theYear.value >2100 || document.form.theYear.value <=1900) {
+//        alert( "Please provide a valid year of birth! eg 2019" );
+//        document.form.theYear.focus() ;
+//        return false;
+//     }
+//     else if( document.form.theMonth.value == "" || isNaN( document.form.theMonth.value ) || 
+//     document.form.theMonth.value.length != 2 || document.form.month.value > 12  || document.form.theMonth.value <= 0){
+//        alert( "Please provide your month of birth! between 1 and 12" );
+//        document.form.month.focus() ;
+//        return false;
+//     }
+//     else if( document.form.theDay.value == "" || isNaN( document.form.theDay.value ) || 
+//     document.form.theDay.value.length != 2|| document.form.theDay.value > 31 || document.form.theDay.value <= 0) {
+//        alert( "Please provide a valid date that you were born in!" );
+//        document.form.day.focus() ;
+//        return false;
+//     }
+//     else if(genders[0].checked==false && genders[1].checked==false ) {
+//         alert("You must select male or female");
+//         return false;
+//     }   
+//     else{
+//       return true ;
+//     }
+    
+//   }
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let userGender = form.gender.value;
@@ -13,7 +42,6 @@ form.addEventListener("submit", (e) => {
     var akaName = compareNames(theYear.value, theMonth.value, theDay.value, userGender)
 
     displayAkan.innerHTML = `Your Akan name is: ${akaName}`;
-
 });
 
 
@@ -25,25 +53,11 @@ function compareNames(year, month, day, gender){
     if (gender === "male"){
         var date = new Date(parseInt(year), parseInt(month), parseInt(day))
         var dayOfWeek = wkday[date.getDay()];
-    
-       
-        return male[date.getDay()]
-    
-        
+     return male[date.getDay()]
     }else{
         var date = new Date(parseInt(year), parseInt(month), parseInt(day))
         var dayOfWeek = wkday[date.getDay()];
-    
-        
         return female[date.getDay()]
-        
     }
-    
-    
-
-  
-
-
-
 
 }
